a7975b01cd11b2b96207f516437c36d4cec05f3e
Craig Gordon <cyghfer@gmail.com>
2019-02-16 22:13:51 -0500

    Add Home navlink + switch chart images from patterns to marker symbols

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,13 +3,16 @@ import styles from './navbar.module.scss';
 
 const NavBar = (props) => (
   <div className={`${styles.container}${props.fixed ? ' ' + styles.fixed : ''}`}>
-    <h5 className={`${styles.link} ${styles.one}${props.current === 1 ? ' ' + styles.current : ''}`}>
+    <h5 className={`${styles.link} ${styles.one}${props.current === 0 ? ' ' + styles.current : ''}`}>
+      <a href='#'>Home</a>
+    </h5>
+    <h5 className={`${styles.link} ${styles.two}${props.current === 1 ? ' ' + styles.current : ''}`}>
       <a href='#about'>About</a>
     </h5>
-    <h5 className={`${styles.link} ${styles.two}${props.current === 2 ? ' ' + styles.current : ''}`}>
+    <h5 className={`${styles.link} ${styles.three}${props.current === 2 ? ' ' + styles.current : ''}`}>
       <a href='#portfolio'>Portfolio</a>
     </h5>
-    <h5 className={`${styles.link} ${styles.three}${props.current === 3 ? ' ' + styles.current : ''}`}>
+    <h5 className={`${styles.link} ${styles.four}${props.current === 3 ? ' ' + styles.current : ''}`}>
       <a href='#contact'>Contact</a>
     </h5>
   </div>
--- src/data/skills.ts
+++ src/data/skills.ts
@@ -1,47 +1,27 @@
 const skillsSeries = [
-  {
-    name: 'Languages',
-    data: [
-      {name: 'JavaScript', value: 90, color: {pattern: {image: 'javascript.svg'}}},
-      {name: 'TypeScript', value: 50, color: {pattern: {image: 'typescript.svg'}}},
-      {name: 'HTML5', value: 80, color: {pattern: {image: 'html5.svg'}}},
-      {name: 'CSS3', value: 80, color: {pattern: {image: 'css3.svg'}}},
-      {name: 'SQL', value: 70, color: {pattern: {image: 'sql.svg'}}}
-    ]
-  },
-  {
-    name: 'Frontend',
-    data: [
-      {name: 'React', value: 90, color: {pattern: {image: 'react.svg'}}},
-      {name: 'jQuery', value: 70, color: {pattern: {image: 'jquery.svg'}}},
-      {name: 'Redux', value: 70, color: {pattern: {image: 'redux.svg'}}},
-      {name: 'Highcharts', value: 80, color: {pattern: {image: 'highcharts.svg'}}},
-      {name: 'Sass', value: 60, color: {pattern: {image: 'sass.svg'}}},
-      {name: 'Styled-Components', value: 70, color: {pattern: {image: 'styledcomponents.svg'}}},
-      {name: 'Bootstrap', value: 70, color: {pattern: {image: 'bootstrap.svg'}}},
-    ]
-  },
-  {
-    name: 'Backend',
-    data: [
-      {name: 'Node', value: 90, color: {pattern: {image: 'node.svg'}}},
-      {name: 'Express', value: 90, color: {pattern: {image: 'express.svg'}}},
-      {name: 'Sequelize', value: 80, color: {pattern: {image: 'sequelize.svg'}}},
-      {name: 'MongoDB', value: 60, color: {pattern: {image: 'mongodb.svg'}}},
-    ]
-  },
-  {
-    name: 'Miscellaneous',
-    data: [
-      {name: 'Mocha', value: 60, color: {pattern: {image: 'mocha.svg'}}},
-      {name: 'Jest', value: 60, color: {pattern: {image: 'jest.svg'}}},
-      {name: 'Git', value: 80, color: {pattern: {image: 'git.svg'}}},
-      {name: 'Webpack', value: 80, color: {pattern: {image: 'webpack.svg'}}},
-      {name: 'RESTful APIs', value: 90, color: {pattern: {image: 'rest.svg'}}},
-      {name: 'Chrome APIs', value: 60, color: {pattern: {image: 'chrome.svg'}}},
-      {name: 'Gatsby', value: 60, color: {pattern: {image: 'gatsby.svg'}}}
-    ]
-  }
-];
+    {marker: {symbol: 'url(javascript.svg)'}, data: [{name: 'JavaScript', value: 90}]},
+    {marker: {symbol: 'url(typescript.svg)'}, data: [{name: 'TypeScript', value: 50}]},
+    {marker: {symbol: 'url(html5.svg)'}, data: [{name: 'HTML5', value: 80}]},
+    {marker: {symbol: 'url(css3.svg)'}, data: [{name: 'CSS3', value: 80}]},
+    {marker: {symbol: 'url(sql.svg)'}, data: [{name: 'SQL', value: 70}]},
+    {marker: {symbol: 'url(react.svg)'}, data: [{name: 'React', value: 90}]},
+    {marker: {symbol: 'url(jquery.svg)'}, data: [{name: 'jQuery', value: 70}]},
+    {marker: {symbol: 'url(redux.svg)'}, data: [{name: 'Redux', value: 70}]},
+    {marker: {symbol: 'url(highcharts.svg)'}, data: [{name: 'Highcharts', value: 80}]},
+    {marker: {symbol: 'url(sass.svg)'}, data: [{name: 'Sass', value: 60}]},
+    {marker: {symbol: 'url(styledcomponents.svg)'}, data: [{name: 'Styled-Components', value: 70}]},
+    {marker: {symbol: 'url(bootstrap.svg)'}, data: [{name: 'Bootstrap', value: 70}]},
+    {marker: {symbol: 'url(node.svg)'}, data: [{name: 'Node', value: 90}]},
+    {marker: {symbol: 'url(express.svg)'}, data: [{name: 'Express', value: 90}]},
+    {marker: {symbol: 'url(sequelize.svg)'}, data: [{name: 'Sequelize', value: 80}]},
+    {marker: {symbol: 'url(mongodb.svg)'}, data: [{name: 'MongoDB', value: 60}]},
+    {marker: {symbol: 'url(mocha.svg)'}, data: [{name: 'Mocha', value: 60}]},
+    {marker: {symbol: 'url(jest.svg)'}, data: [{name: 'Jest', value: 60}]},
+    {marker: {symbol: 'url(git.svg)'}, data: [{name: 'Git', value: 80}]},
+    {marker: {symbol: 'url(webpack.svg)'}, data: [{name: 'Webpack', value: 80}]},
+    {marker: {symbol: 'url(rest.svg)'}, data: [{name: 'RESTful APIs', value: 90}]},
+    {marker: {symbol: 'url(chrome.svg)'}, data: [{name: 'Chrome APIs', value: 60}]},
+    {marker: {symbol: 'url(gatsby.svg)'}, data: [{name: 'Gatsby', value: 60}]}
+  ];
 
 export default skillsSeries;
